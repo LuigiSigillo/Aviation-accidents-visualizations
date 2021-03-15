8bac756b21446bd92f2520510d81e8a60685c06a
LuigiSigillo <luigisigillo30@gmail.com>
2021-03-15 13:13:31 +0100

    mancano nuovi dati

--- map.js
+++ map.js
@@ -1,197 +1,179 @@
+d3.csv("Aviation cleaned.csv", function (err, data) {
 
-/*  This visualization was made possible by modifying code provided by:
+    var config = { "color1": "#d3e5ff", "color2": "#08306B", "stateDataColumn": "state_or_territory", "valueDataColumn": "population_estimate_for_july_1_2013_number" }
 
-Scott Murray, Choropleth example from "Interactive Data Visualization for the Web" 
-https://github.com/alignedleft/d3-book/blob/master/chapter_12/05_choropleth.html   
-		
-Malcolm Maclean, tooltips example tutorial
-http://www.d3noob.org/2013/01/adding-tooltips-to-d3js-graph.html
+    var WIDTH = 800, HEIGHT = 500;
 
-Mike Bostock, Pie Chart Legend
-http://bl.ocks.org/mbostock/3888852  */
+    var COLOR_COUNTS = 9;
 
-		
-//Width and height of map
-var width = 960;
-var height = 500;
+    var SCALE = 0.7;
 
-// D3 Projection
-var projection = d3.geo.albersUsa()
-				   .translate([width/2, height/2])    // translate to center of screen
-				   .scale([1000]);          // scale things down so see entire US
-        
-// Define path generator
-var path = d3.geo.path()               // path generator that will convert GeoJSON to SVG paths
-		  	 .projection(projection);  // tell path generator to use albersUsa projection
-
-		
-// Define linear scale for output
-var color = d3.scale.linear()
-			  .range(["rgb(213,222,217)","rgb(69,173,168)","rgb(84,36,55)","rgb(217,91,67)"]);
-
-var legendText = ["Cities Lived", "States Lived", "States Visited", "Nada"];
-
-//Create SVG element and append map to the SVG
-var svg = d3.select("body")
-			.append("svg")
-			.attr("width", width)
-			.attr("height", height);
-        
-// Append Div for tooltip to SVG
-var div = d3.select("body")
-		    .append("div")   
-    		.attr("class", "tooltip")               
-    		.style("opacity", 0);
-
-
-
-function haversineDistance(coords1, coords2, isMiles) {
-    function toRad(x) {
-        return x * Math.PI / 180;
+    function Interpolate(start, end, steps, count) {
+        var s = start,
+            e = end,
+            final = s + (((e - s) / steps) * count);
+        return Math.floor(final);
     }
-    
-    var lon1 = coords1[0];
-    var lat1 = coords1[1];
-    
-    var lon2 = coords2[0];
-    var lat2 = coords2[1];
-    
-    var R = 6371; // km
-    
-    var x1 = lat2 - lat1;
-    var dLat = toRad(x1);
-    var x2 = lon2 - lon1;
-    var dLon = toRad(x2)
-    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
-        Math.sin(dLon / 2) * Math.sin(dLon / 2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c;
-    
-    if(isMiles) d /= 1.60934;
-    
-    return d;
-}
 
+    function Color(_r, _g, _b) {
+        var r, g, b;
+        var setColors = function (_r, _g, _b) {
+            r = _r;
+            g = _g;
+            b = _b;
+        };
 
-function nearestState(coordString,state){
-    //"39.408889,-89.990277"
-    var coordArray = coordString.split(",")
-    var coordCrash= [parseFloat(coordArray[0]),parseFloat(coordArray[1])]
-    var coordState = [parseFloat(state.longitude),parseFloat(state.latitude)]
-    var distance = haversineDistance(coordCrash,coordState)
-    return distance
-}
-// Load in my states data!
-d3.csv("Aviation cleaned.csv", function(data) {
-color.domain([0,1,2,3]); // setting the range of the input data
+        setColors(_r, _g, _b);
+        this.getColors = function () {
+            var colors = {
+                r: r,
+                g: g,
+                b: b
+            };
+            return colors;
+        };
+    }
 
-// Load GeoJSON data and merge with states data
-d3.json("us-states.json", function(json) {
-// Loop through each state data value in the .csv file
-for (var i = 0; i < data.length; i++) {
+    function hexToRgb(hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+        } : null;
+    }
 
-	// Grab State Name
-	var dataCoordinates = data[i]["Crash.Location"]; //"39.408889,-89.990277"
-    
-	// Grab data value 
-	var dataValue = data[i]["Total.Fatal.Injuries"];
-    var distanceState = Infinity
-	// Find the corresponding state inside the GeoJSON
-	for (var j = 0; j < json.length; j++)  {
-		//var jsonState = json.features[j].properties.state;
-        distanceStateNew = nearestState(dataCoordinates,json[j])
-		if (distanceState > distanceStateNew) {
-            distanceState = distanceStateNew
-		// Copy the data value into the JSON
-		json[j].Fatal += dataValue; 
-		}
-	}
-}
-		
-// Bind the data to the SVG and create one path per GeoJSON feature
-svg.selectAll("path")
-	.data(json)
-	.enter()
-	.append("path")
-	.attr("d", path)
-	.style("stroke", "#fff")
-	.style("stroke-width", "1")
-	.style("fill", function(d) {
+    function valueFormat(d) {
+        if (d > 1000000000) {
+            return Math.round(d / 1000000000 * 10) / 10 + "B";
+        } else if (d > 1000000) {
+            return Math.round(d / 1000000 * 10) / 10 + "M";
+        } else if (d > 1000) {
+            return Math.round(d / 1000 * 10) / 10 + "K";
+        } else {
+            return d;
+        }
+    }
 
-	// Get data value
-	var value = d.Fatal;
+    var COLOR_FIRST = config.color1, COLOR_LAST = config.color2;
 
-	if (value) {
-	//If value exists…
-	return color(value);
-	} else {
-	//If value is undefined…
-	return "rgb(213,222,217)";
-	}
-});
+    var rgb = hexToRgb(COLOR_FIRST);
 
-	/*	 
-// Map the cities I have lived in!
-d3.csv("cities-lived.csv", function(data) {
+    var COLOR_START = new Color(rgb.r, rgb.g, rgb.b);
 
-svg.selectAll("circle")
-	.data(data)
-	.enter()
-	.append("circle")
-	.attr("cx", function(d) {
-		return projection([d.lon, d.lat])[0];
-	})
-	.attr("cy", function(d) {
-		return projection([d.lon, d.lat])[1];
-	})
-	.attr("r", function(d) {
-		return Math.sqrt(d.years) * 4;
-	})
-		.style("fill", "rgb(217,91,67)")	
-		.style("opacity", 0.85)	
+    rgb = hexToRgb(COLOR_LAST);
+    var COLOR_END = new Color(rgb.r, rgb.g, rgb.b);
 
-	// Modification of custom tooltip code provided by Malcolm Maclean, "D3 Tips and Tricks" 
-	// http://www.d3noob.org/2013/01/adding-tooltips-to-d3js-graph.html
-	.on("mouseover", function(d) {      
-    	div.transition()        
-      	   .duration(200)      
-           .style("opacity", .9);      
-           div.text(d.place)
-           .style("left", (d3.event.pageX) + "px")     
-           .style("top", (d3.event.pageY - 28) + "px");    
-	})   
+    var MAP_STATE = config.stateDataColumn;
+    var MAP_VALUE = config.valueDataColumn;
 
-    // fade out tooltip on mouse out               
-    .on("mouseout", function(d) {       
-        div.transition()        
-           .duration(500)      
-           .style("opacity", 0);   
+    var width = WIDTH,
+        height = HEIGHT;
+
+    var valueById = d3.map();
+
+    var startColors = COLOR_START.getColors(),
+        endColors = COLOR_END.getColors();
+
+    var colors = [];
+
+    for (var i = 0; i < COLOR_COUNTS; i++) {
+        var r = Interpolate(startColors.r, endColors.r, COLOR_COUNTS, i);
+        var g = Interpolate(startColors.g, endColors.g, COLOR_COUNTS, i);
+        var b = Interpolate(startColors.b, endColors.b, COLOR_COUNTS, i);
+        colors.push(new Color(r, g, b));
+    }
+
+    var quantize = d3.scale.quantize()
+        .domain([0, 1.0])
+        .range(d3.range(COLOR_COUNTS).map(function (i) { return i }));
+
+    var path = d3.geo.path();
+
+    var svg = d3.select("#canvas-svg").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    d3.tsv("https://gist.githubusercontent.com/amartone/5e9a82772cf1337d688fe47729e99532/raw/65a04d5b4934beda724630f18c475d350628f64d/us-state-names.tsv", function (error, names) {
+
+        name_id_map = {};
+        id_name_map = {};
+
+        for (var i = 0; i < names.length; i++) {
+            name_id_map[names[i].name] = names[i].id;
+            id_name_map[names[i].id] = names[i].name;
+        }
+
+        data.forEach(function (d) {
+            var id = name_id_map[d['Crash.Country']];
+            valueById.set(id, +d["Total.Fatal.Injuries"]);
+            console.log(id,d["Total.Fatal.Injuries"])
+        });
+
+        quantize.domain([d3.min(data, function (d) { return +d[MAP_VALUE] }),
+        d3.max(data, function (d) { return +d[MAP_VALUE] })]);
+
+        d3.json("us-states.json", function (error, us) {
+            svg.append("g")
+                .attr("class", "states-choropleth")
+                .selectAll("path")
+                .data(topojson.feature(us, us.objects.states).features)
+                .enter().append("path")
+                .attr("transform", "scale(" + SCALE + ")")
+                .style("fill", function (d) {
+                    if (valueById.get(d.id)) {
+                        var i = quantize(valueById.get(d.id));
+                        var color = colors[i].getColors();
+                        return "rgb(" + color.r + "," + color.g +
+                            "," + color.b + ")";
+                    } else {
+                        return "";
+                    }
+                })
+                .attr("d", path)
+                .on("mousemove", function (d) {
+                    var html = "";
+
+                    html += "<div class=\"tooltip_kv\">";
+                    html += "<span class=\"tooltip_key\">";
+                    html += id_name_map[d.id];
+                    html += "</span>";
+                    html += "<span class=\"tooltip_value\">";
+                    html += (valueById.get(d.id) ? valueFormat(valueById.get(d.id)) : "");
+                    html += "";
+                    html += "</span>";
+                    html += "</div>";
+
+                    $("#tooltip-container").html(html);
+                    $(this).attr("fill-opacity", "0.8");
+                    $("#tooltip-container").show();
+
+                    var coordinates = d3.mouse(this);
+
+                    var map_width = $('.states-choropleth')[0].getBoundingClientRect().width;
+
+                    if (d3.event.layerX < map_width / 2) {
+                        d3.select("#tooltip-container")
+                            .style("top", (d3.event.layerY + 15) + "px")
+                            .style("left", (d3.event.layerX + 15) + "px");
+                    } else {
+                        var tooltip_width = $("#tooltip-container").width();
+                        d3.select("#tooltip-container")
+                            .style("top", (d3.event.layerY + 15) + "px")
+                            .style("left", (d3.event.layerX - tooltip_width - 30) + "px");
+                    }
+                })
+                .on("mouseout", function () {
+                    $(this).attr("fill-opacity", "1.0");
+                    $("#tooltip-container").hide();
+                });
+
+            svg.append("path")
+                .datum(topojson.mesh(us, us.objects.states, function (a, b) { return a !== b; }))
+                .attr("class", "states")
+                .attr("transform", "scale(" + SCALE + ")")
+                .attr("d", path);
+        });
+
     });
-});  
-        */
-// Modified Legend Code from Mike Bostock: http://bl.ocks.org/mbostock/3888852
-var legend = d3.select("body").append("svg")
-      			.attr("class", "legend")
-     			.attr("width", 140)
-    			.attr("height", 200)
-   				.selectAll("g")
-   				.data(color.domain().slice().reverse())
-   				.enter()
-   				.append("g")
-     			.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-  	legend.append("rect")
-   		  .attr("width", 18)
-   		  .attr("height", 18)
-   		  .style("fill", color);
-
-  	legend.append("text")
-  		  .data(legendText)
-      	  .attr("x", 24)
-      	  .attr("y", 9)
-      	  .attr("dy", ".35em")
-      	  .text(function(d) { return d; });
-	});
-
 });
