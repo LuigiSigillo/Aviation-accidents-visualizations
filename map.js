622bf9838ffc2ebc2a5a2eefa2a678c12f11e729
LuigiSigillo <luigisigillo30@gmail.com>
2021-03-17 12:57:26 +0100

    slider funzionante

--- map.js
+++ map.js
@@ -83,20 +83,37 @@ d3.csv("AviationCrashLocation.csv", function (err, data) {
             id_name_map[names[i].id] = names[i].name;
         }
 
+        
 
-        var e = d3.nest()
-            .key(function (d) { return d[dbNames.crashCountry]; })
-            .rollup(function (v) {
-                return {
-                    Total_Accidents:d3.sum(v, function (d) { return 1;}),
-                    Fatalities: d3.sum(v, function (d) { return d[dbNames.fatal]; }),
-                    Serious_Injuries: d3.sum(v, function (d) { return d[dbNames.serious] }),
-                    Minor_Injuries: d3.sum(v, function (d) { return d[dbNames.minor] })
-                };
-            })
-            .map(data);
+        function change(year) {
 
+            var e = d3.nest()
+                .key(function (d) { return d[dbNames.crashCountry]; })
+                .rollup(function (v) {
+                    return {
+                        Total_Accidents: d3.sum(v, function (d) {
+                            if (year >= +d["Event.Date"].split("-")[0]) return 1;
+                            else return 0;
+                        }),
+                        Fatalities: d3.sum(v, function (d) {
+                            if (year >= +d["Event.Date"].split("-")[0]) return d[dbNames.fatal];
+                            else return 0;
+                        }),
+                        Serious_Injuries: d3.sum(v, function (d) {
+                            if (year >= +d["Event.Date"].split("-")[0]) return d[dbNames.serious];
+                            else return 0;
+                        }),
+                        Minor_Injuries: d3.sum(v, function (d) {
+                            if (year >= +d["Event.Date"].split("-")[0]) return d[dbNames.minor];
+                            else return 0;
+                        })
+                    };
+                })
+                .map(data);
+            return e
+        }
 
+        e = change(1997)
         d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json", function (error, us) {
             svg.append("g")
                 .attr("class", "states-choropleth")
@@ -205,48 +222,85 @@ d3.csv("AviationCrashLocation.csv", function (err, data) {
                     console.log(d)
                     return d;
                 });
-
-        });
-        /////////////////////////////////////////////////////////
-        function processData(year) {
-            var newData = {};
-            data.forEach(function (d) {
-                if (!(d['Crash.Country'] in newData)) {
-                    //2020-05-02
-                    if (+d["Event.Date"].split("-")[0] <= +year) {
-                        newData[d['Crash.Country']] = { count: 1, coord: [+d["Crash.Location"].split(",")[0], +d["Crash.Location"].split(",")[1]] };
-                    }
-                    else {
-                        newData[d['Crash.Country']] = { count: 0, coord: [+d["Crash.Location"].split(",")[0], +d["Crash.Location"].split(",")[1]] };
-                    }
-                }
-                else if (d['Crash.Country'] in newData && +d["Event.Date"].split("-")[0] <= +year) {
-                    newData[d['Crash.Country']].count = newData[d['Crash.Country']].count + 1;
-                }
-            })
-            return newData;
-        }
-
-        d3.select("input")
-            .on("change", function () {
-                var yearInput = +d3.select(this).node().value;
-                newData = processData(yearInput);
-                //cambia colore
-                svg.selectAll("g")
-                    .data(d3.keys(newData))
-                    .transition()
-                    .duration(500)
+                d3.select("input")
+                .on("change", function () {
+                    var yearInput = +d3.select(this).node().value;
+                    e = change(yearInput);
+                    //cambia colore
+                    svg.append("g")
+                    .attr("class", "states-choropleth")
+                    .selectAll("path")
+                    .data(topojson.feature(us, us.objects.states).features)
+                    .enter().append("path")
+                    .attr("transform", "scale(" + params.SCALE + ")")
                     .style("fill", function (d) {
                         if (e[id_name_map[d.id]] != undefined) {
                             var color = colorMapping(e[id_name_map[d.id]]["Fatalities"]);
-
+    
                             //console.log(i, valueById.get(d.id))
                             return "rgb(" + color.r + "," + color.g + "," + color.b + ")";
                         } else {
                             return "";
                         }
+                    })
+                    .attr("d", path)
+                    .on("mousemove", function (d) {
+                        var html = "";
+                        html += "<div class=\"tooltip_kv\">";
+                        html += "<span class=\"tooltip_key\">";
+                        html += id_name_map[d.id];
+                        html += "</span><br>";
+                        html += "<span class=\"tooltip_value\">";
+                        html += "<a>Total Accidents: "
+                        html += (e[id_name_map[d.id]]["Total_Accidents"]);
+                        html += "</a>";
+                        html += "</span><br>";
+                        html += "<span class=\"tooltip_value\">";
+                        html += "<a>Fatalities: "
+                        html += (e[id_name_map[d.id]]["Fatalities"]);
+                        html += "</a>";
+                        html += "</span><br>";
+                        html += "<span class=\"tooltip_value\">";
+                        html += "<a>Serious Injuries: "
+                        html += (e[id_name_map[d.id]]["Serious_Injuries"]);
+                        html += "</a>";
+                        html += "</span><br>";
+                        html += "<span class=\"tooltip_value\">";
+                        html += "<a>Minor Injuries: "
+                        html += (e[id_name_map[d.id]]["Minor_Injuries"]);
+                        html += "</a>";
+                        html += "</span>";
+                        html += "</div>";
+    
+                        $("#tooltip-container").html(html);
+                        $(this).attr("fill-opacity", "0.8");
+                        $("#tooltip-container").show();
+    
+                        var coordinates = d3.mouse(this);
+    
+                        var map_width = $('.states-choropleth')[0].getBoundingClientRect().width;
+    
+                        if (d3.event.layerX < map_width / 2) {
+                            d3.select("#tooltip-container")
+                                .style("top", (d3.event.layerY + 15) + "px")
+                                .style("left", (d3.event.layerX + 15) + "px");
+                        } else {
+                            var tooltip_width = $("#tooltip-container").width();
+                            d3.select("#tooltip-container")
+                                .style("top", (d3.event.layerY + 15) + "px")
+                                .style("left", (d3.event.layerX - tooltip_width - 30) + "px");
+                        }
+                    })
+                    .on("mouseout", function () {
+                        $(this).attr("fill-opacity", "1.0");
+                        $("#tooltip-container").hide();
                     });
-            });
+    
+                });
+        });
+        /////////////////////////////////////////////////////////
+
+        
     });
 
 
