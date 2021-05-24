a93455334000542e9d8b7d93858736e91ae16f05
Mattia <m.pocci@araneum.it>
2021-05-24 10:25:41 +0200

    pause button

--- scripts/playbutton.js
+++ scripts/playbutton.js
@@ -1,31 +1,30 @@
 var playButton = d3.select("#play-button");
+var pauseButton = d3.select("#pause-button");
+var globalone = undefined
 
+function myStopFunction(myVar) {
+    clearInterval(myVar);
+}
+
+function rec_play() {
+    var slider = document.getElementById("slider");
+    if (slider.value >= 2020) {
+        myStopFunction();
+        return;
+    }
+    var output = document.getElementById("demo");
+    slider.value = +slider.value + 1
+    output.innerHTML = slider.value;
+    var evt = document.createEvent("HTMLEvents");
+    evt.initEvent("change", false, true);
+    slider.dispatchEvent(evt)
+    console.log('slider ', slider.value)
+}
 
 playButton.on("click", function () {
-
-    var myVar = setInterval(rec_play, 1000);
-    
-
-    
-    function myStopFunction() {
-      clearInterval(myVar);
-    }
-
-    function rec_play() {
-        var slider = document.getElementById("slider");
-        if(slider.value>=2020){
-            myStopFunction();
-            return;
-        }
-        var output = document.getElementById("demo");
-        slider.value = +slider.value + 1
-        output.innerHTML = slider.value;
-        var evt = document.createEvent("HTMLEvents");
-        evt.initEvent("change", false, true);
-        slider.dispatchEvent(evt)
-        console.log('slider ', slider.value)
-    }
-
+    globalone = setInterval(rec_play, 1000);
 })
 
-
+pauseButton.on("click", function () {
+    myStopFunction(globalone);
+})
