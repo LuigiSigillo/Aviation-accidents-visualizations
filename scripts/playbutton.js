6c715a246b7dcfba0269c612ff616b69de198923
Mattia <m.pocci@araneum.it>
2021-05-24 11:07:45 +0200

    foobar

--- scripts/playbutton.js
+++ scripts/playbutton.js
@@ -1,7 +1,7 @@
 var playButton = d3.select("#play-button");
 var pauseButton = d3.select("#pause-button");
 var globalone = undefined
-
+//foobar
 function myStopFunction(myVar) {
     clearInterval(myVar);
 }
